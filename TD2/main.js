be53f5dcba1fe2e0ff990873759d54508dbca2a6
rl303414 <leong-foc-sing-andhy.raharison@etu.unice.fr>
2025-10-01 19:50:38 +0200

    Ajout d'un objet 3D

--- TD2/main.js
+++ TD2/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Création de la scène et de la caméra
 const scene = new THREE.Scene();
@@ -10,14 +11,31 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
+// AJout d'un modèle 3D
+const loader3d = new GLTFLoader();
+
+loader3d.load( 'sasuke_uchiha/scene.gltf', function ( gltf ) {
+
+  gltf.scene.scale.set(0.02, 0.02, 0.02)
+  gltf.scene.position.set(0, 1, 0)
+  scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
+
+
 // Ajout de la lumière uniformément
 const ambientColor = 0xFFFFFF;
 const ambientIntensity = 1;
 const ambientLight = new THREE.AmbientLight(ambientColor, ambientIntensity);
 scene.add(ambientLight);
 
+
 //Ajout d'une lumière qui pointe que sur une direction
-const directionalColor = 0xffaa00;
+const directionalColor = 0xFFFFFF;
 const directionalIntensity = 5;
 //const directionalDistance = 10; // C'est la distance jusqu'où la lumière éclaire
 //const directionalDecay = 2; // Décroissance de l'intensité avec la distance
@@ -38,7 +56,7 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 // Position de la caméra
-camera.position.z = 5;
+camera.position.set(0, 2, 5);
 
 // Fonction pour faire rotationner le cube
 function animate() {
